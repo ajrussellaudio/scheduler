5107e0d2be566da7241154aef5ac698f1334cca7
Alan Russell <ajrussellaudio@gmail.com>
2022-10-17 09:09:55 +0100

    Add basic calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
-function App() {
+import './App.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+export function App() {
+  const events = [
+    {
+      start: moment().toDate(),
+      end: moment().add(1, 'days').toDate(),
+      title: 'Some title'
+    },
+    {
+      start: moment().add(1, 'weeks').toDate(),
+      end: moment().add(1, 'weeks').add(1, 'days').toDate(),
+      title: 'Some other title'
+    }
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        events={events}
+        style={{ height: '100vh' }}
+      />
     </div>
   );
 }
